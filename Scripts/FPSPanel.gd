ad13ba4cf0a9f7f6db6388a575fe26b6684b19f1
aliaslion <alias.lion@posteo.de>
2025-11-11 17:15:37 +0100

    Improved info panel

--- Scripts/FPSPanel.gd
+++ Scripts/FPSPanel.gd
@@ -1,28 +1,20 @@
-extends Panel
+extends Node
 
-onready var fps = $GridContainer/FPS
-onready var x_position = $GridContainer/XPosition
-onready var y_position = $GridContainer/YPosition
-onready var z_position = $GridContainer/ZPosition
-onready var x_rotation = $GridContainer/XRotation
-onready var y_rotation = $GridContainer/YRotation
-onready var z_rotation = $GridContainer/ZRotation
+onready var fps_label = $GridContainer/FPS
+onready var pos_label = $GridContainer/Position
+onready var rot_label = $GridContainer/Rotation
 var vehicle = null
 var position = Vector3.ZERO
 var rotation = Vector3.ZERO
 
 func _process(_delta):
-	fps.text = str(Engine.get_frames_per_second())
+	fps_label.text = "%d" % Engine.get_frames_per_second()
 	if vehicle != null:
 		position = vehicle.translation
 		rotation = vehicle.rotation_degrees
-	else: vehicle = set_vehicle()
-	x_position.text = "%.5f" % position.x
-	y_position.text = "%.5f" % position.y
-	z_position.text = "%.5f" % position.z
-	x_rotation.text = "%.5f" % rotation.x
-	y_rotation.text = "%.5f" % rotation.y
-	z_rotation.text = "%.5f" % rotation.z
+	else: set_vehicle()
+	pos_label.text = "(%8.2f, %8.2f, %8.2f)" % [position.x, position.y, position.z]
+	rot_label.text = "(%8.2f, %8.2f, %8.2f)" % [rotation.x, rotation.y, rotation.z]
 
 func set_vehicle():
 	vehicle = get_node("../../Vehicles").get_vehicle()
