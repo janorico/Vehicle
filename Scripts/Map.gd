da3f4434b5c5cf52894a371e88b72a4570be484a
aliaslion <alias.lion@posteo.de>
2026-08-05 14:23:28 +0200

    Better map (directional markers)

--- Scripts/Map.gd
+++ Scripts/Map.gd
@@ -1,53 +1,51 @@
-extends Control
+extends TextureRect
 
 
-var ready_to_draw: bool = false
-var map_texture: Texture
+const NAVIGATION_ARROW = preload("res://Assets/2D/NavigationArrow.svg")
 var map_divider: float
-var map_size: Vector2 = Vector2.ZERO
-var center_x: float
-var center_y: float
+var players := {}
 
 
-func _initialize():
-	map_texture = load(Global.worlds[Global.world].map_path)
+func _ready() -> void:
+	texture = load(Global.worlds[Global.world].map_path)
 	map_divider = Global.worlds[Global.world].map_divider
-	map_size = map_texture.get_size()
-	center_x = (map_size.x / 2)
-	center_y = ((map_size.y / 2))
-	ready_to_draw = true
-
-
-func _process(_delta):
-	update()
-
-
-func get_vehicles():
-	return get_vehicles_node().get_children()
-
-
-func get_vehicles_node():
-	return get_node("../../../Vehicles")
-
-
-func _draw():
-	if not ready_to_draw:
-		return
-	draw_rect(Rect2(0, 0, map_size.x, map_size.y), Color.black)
-	draw_texture(map_texture, Vector2(0, 0))
 	if Net.is_offline:
-		draw_circle(get_circle_position(get_vehicles_node().get_vehicle().translation), 3, Color.red)
+		create_player("Spatial")
 	else:
-		for vehicle in get_vehicles():
-			vehicle = vehicle.get_child(0)
-			var position = vehicle.get_translation()
-			if vehicle.is_master:
-				draw_circle(get_circle_position(position), 3, Color.red)
-			else:
-				draw_circle(get_circle_position(position), 3, Color.blue)
+		create_player(Net.net_id)
+		for id in Net.player_info:
+			create_player(id)
+		Net.connect("player_connected", self, "create_player")
+		Net.connect("player_disconnected", self, "remove_player")
 
 
-func get_circle_position(vehicle_position: Vector3):
-	var x = Global.inverse(vehicle_position.x) / map_divider
-	var y = Global.inverse(vehicle_position.z) / map_divider
-	return Vector2((center_x + x), (center_y + y))
+func _process(_delta: float) -> void:
+	for id in players:
+		var s = players[id]
+		var p = get_node("../../../Vehicles/%s" % id)
+		if p:
+			var v: Spatial = p.get_child(0)
+			s.position = world2map(v.position)
+			s.rotation = v.transform.basis.inverse().get_euler().y
+
+
+func create_player(id) -> void:
+	var s = Sprite.new()
+	s.name = str(id)
+	s.texture = NAVIGATION_ARROW
+	if not Net.is_offline:
+		s.modulate = Color.red if id == Net.net_id else Color.blue
+	add_child(s)
+	players[id] = s
+
+
+func remove_player(id) -> void:
+	players[id].queue_free()
+	players.erase(id)
+
+
+func world2map(vehicle_position: Vector3) -> Vector2:
+	return Vector2(
+		(-vehicle_position.x) / map_divider,
+		(-vehicle_position.z) / map_divider
+	) + (rect_size / 2)
